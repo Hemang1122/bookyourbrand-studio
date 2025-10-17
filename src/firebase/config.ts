bbe7e68c5c201aaa65ea48da8b3ab557dab2e448
Aakashi Campus <aakashicampus261@gmail.com>
2025-10-17 20:04:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6449361728-f6242",
+  "appId": "1:372067900151:web:f47cf063e692558351d1f2",
+  "apiKey": "AIzaSyBB7WY7Jt5Zz0RuRuxmb0BwNd5VWR08IPA",
+  "authDomain": "studio-6449361728-f6242.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "372067900151"
+};
