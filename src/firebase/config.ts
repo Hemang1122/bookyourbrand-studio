de8c2b761ce172d12aa80625a6629f9caf20b46f
Aakashi Campus <aakashicampus261@gmail.com>
2025-10-19 16:38:42 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,6 @@ export const firebaseConfig = {
   "appId": "1:372067900151:web:f47cf063e692558351d1f2",
   "apiKey": "AIzaSyBB7WY7Jt5Zz0RuRuxmb0BwNd5VWR08IPA",
   "authDomain": "studio-6449361728-f6242.firebaseapp.com",
-  "storageBucket": "studio-6449361728-f6242.appspot.com",
-  "measurementId": "",
+  "measurementId": "G-DXH4MMCK56",
   "messagingSenderId": "372067900151"
 };
