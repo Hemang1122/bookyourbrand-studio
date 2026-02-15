2651ad3364764561a40a201ba9463259da1eb187
Aakashi Campus <aakashicampus261@gmail.com>
2026-02-15 20:02:45 +0000

    There is a syntax error in src/app/(app)/scrum/page.tsx: "Unexpected tok

--- src/app/(app)/scrum/page.tsx
+++ src/app/(app)/scrum/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import React, { useState, useMemo, useEffect } from 'react';
@@ -107,7 +106,7 @@ const ScrumUpdateCard = ({ update, user, isExpanded, onToggleExpand }: { update:
       </div>
     </Collapsible>
   );
-};
+}
 
 
 export default function ScrumPage() {
