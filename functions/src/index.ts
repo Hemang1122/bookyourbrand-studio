d6011d8f1709354c601c165a4f159a6e59abdff9
Aakashi Campus <aakashicampus261@gmail.com>
2026-02-16 22:04:55 +0000

    In functions/src/index.ts, find the onProjectStatusCompleted function a

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -250,7 +250,7 @@ export const onProjectStatusCompleted = onDocumentUpdated(
     await db.collection('projects').doc(projectId).update({ approvalToken });
     
     // Build approval URLs
-    const baseUrl = 'https://studio--studio-6449361728-f6242.us-central1.hosted.app';
+    const baseUrl = 'https://studio-6449361728-f6242.web.app';
     const approveUrl = `${baseUrl}/api/project-approval?projectId=${projectId}&action=approve&token=${approvalToken}`;
     const changesUrl = `${baseUrl}/api/project-approval?projectId=${projectId}&action=changes&token=${approvalToken}`;
     
